40e6bf3bb9b43008ebc2049dd9be76822ae9c1b2
greenslonik10 <103117871+greenslonik10@users.noreply.github.com>
2022-05-20 15:56:49 +0300

    Add files via upload

---
+++ Laba_1/UI/FormStar.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WinFormsApp1.UI
+{
+    partial class FormStar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.numericUpDownAge = new System.Windows.Forms.NumericUpDown();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.numericUpDownTemperature = new System.Windows.Forms.NumericUpDown();
+            this.labelTemperature = new System.Windows.Forms.Label();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTemperature)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCancel);
+            this.panel1.Controls.Add(this.buttonOK);
+            this.panel1.Controls.Add(this.numericUpDownAge);
+            this.panel1.Controls.Add(this.labelAge);
+            this.panel1.Controls.Add(this.numericUpDownTemperature);
+            this.panel1.Controls.Add(this.labelTemperature);
+            this.panel1.Controls.Add(this.textBoxWeight);
+            this.panel1.Controls.Add(this.labelWeight);
+            this.panel1.Controls.Add(this.textBoxName);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(369, 220);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(206, 167);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(94, 29);
+            this.buttonCancel.TabIndex = 10;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(54, 167);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(94, 29);
+            this.buttonOK.TabIndex = 9;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // numericUpDownAge
+            // 
+            this.numericUpDownAge.Location = new System.Drawing.Point(173, 108);
+            this.numericUpDownAge.Maximum = new decimal(new int[] {
+            100000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAge.Name = "numericUpDownAge";
+            this.numericUpDownAge.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDownAge.TabIndex = 8;
+            this.numericUpDownAge.Value = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(49, 110);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(64, 20);
+            this.labelAge.TabIndex = 7;
+            this.labelAge.Text = "Возраст";
+            // 
+            // numericUpDownTemperature
+            // 
+            this.numericUpDownTemperature.Location = new System.Drawing.Point(173, 75);
+            this.numericUpDownTemperature.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDownTemperature.Name = "numericUpDownTemperature";
+            this.numericUpDownTemperature.Size = new System.Drawing.Size(150, 27);
+            this.numericUpDownTemperature.TabIndex = 6;
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Location = new System.Drawing.Point(49, 77);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(100, 20);
+            this.labelTemperature.TabIndex = 5;
+            this.labelTemperature.Text = "Температура";
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Location = new System.Drawing.Point(173, 41);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(150, 27);
+            this.textBoxWeight.TabIndex = 3;
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Location = new System.Drawing.Point(49, 44);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(33, 20);
+            this.labelWeight.TabIndex = 2;
+            this.labelWeight.Text = "Вес";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(173, 8);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(150, 27);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(49, 11);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(77, 20);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Название";
+            // 
+            // FormStar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 220);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormStar";
+            this.Text = "Звезда";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTemperature)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.NumericUpDown numericUpDownTemperature;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.NumericUpDown numericUpDownAge;
+        private System.Windows.Forms.Label labelAge;
+    }
+}
---
+++ Laba_1/businessModel/Moon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1.businessModel
+{
+    public class Moon : SpaceObject
+    {     
+        private float density;
+        private float orbitRadius;
+        public float Density { get { return density; } set { density = value; } }
+        public float OrbitRadius { get { return orbitRadius; } set { orbitRadius = value; } }
+        public Moon() : base()
+        {
+            density = 0;
+            orbitRadius = 0;
+        }
+    }
+}
