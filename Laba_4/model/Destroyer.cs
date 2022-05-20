17b303fa9599e27521619165792cab66d220cae9
greenslonik10 <103117871+greenslonik10@users.noreply.github.com>
2022-05-20 16:00:05 +0300

    Add files via upload

---
+++ Laba_4/model/Destroyer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShip.model
+{
+    class Destroyer : MilitaryShip
+    {
+        // количество ракет в ЗРК
+        private int salvoMissileCount;
+        // количество ракет на борту
+        private int missileCount;
+        // вес одной ракеты
+        private int missileWeight;
+
+        public int SalvoMissileCount { get { return salvoMissileCount; } set { salvoMissileCount = value; } }
+        public int MissileCount { get { return missileCount; } set { missileCount = value; } }
+        public int MissileWeight { get { return missileWeight; } set { missileWeight = value; } }
+
+        public Destroyer()
+        {
+            this.salvoMissileCount = 0;
+            this.missileCount = 0;
+            this.missileWeight = 0;
+        }
+
+        // сделать залп
+        public bool TrySalvo()
+        {
+            if ((missileCount > 0) && (missileCount >= salvoMissileCount))
+            {
+                missileCount -= salvoMissileCount;
+                Loading -= (salvoMissileCount * missileWeight);
+                return true;
+            }
+            return false;
+        }
+
+        // перезарядить
+        public bool TryReloadSalvo()
+        {
+            if (TryLoad(salvoMissileCount * missileWeight))
+            {
+                missileCount++;
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + Environment.NewLine +
+                "Количество ракет в залпе: " +salvoMissileCount + Environment.NewLine +
+                "Количество ракет на борту: " + missileCount + Environment.NewLine +
+                "Вес ракеты: " + missileWeight;
+        }
+    }
+}
---
+++ Laba_4/model/IShip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShip.model
+{
+    interface IShip
+    {
+        int Loading { get; set; }
+
+        // загрузить на величину weight
+        bool TryLoad(int weight);
+
+        // разгрузить на величину weight
+        bool TryUnload(int weight);
+
+    }
+}
